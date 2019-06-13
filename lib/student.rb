c088e05d9e93f02f58ad751f43cf4285c303ceb4
Sara Harvey <saraharveynyc@gmail.com>
2019-06-13 15:46:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Student
 
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
@@ -7,8 +8,8 @@ class Student
   def initialize(student_hash)
     student_hash.each do |k,v|
       self.send("#{k}=", v)
-    end 
-    @@all << self 
+    end
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
